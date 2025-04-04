9f47f639b41fd05b71d791e8974b3dbd5d8ab4dc
aymane <aymane.bagari@gmail.com>
2025-04-04 02:53:20 +0200

    test

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,12 +1,12 @@
 import unittest
 from unittest import mock
-from etl.ingest import scrape_books_to_scrape  # Import your scraping function here
-from bs4 import BeautifulSoup
+from etl.ingest import scrape_books_to_scrape  # Import your function here
 
 class TestScrapingFunction(unittest.TestCase):
 
     @mock.patch('requests.get')  # Mock requests.get to avoid actual HTTP requests
-    def test_scrape_books_to_scrape(self, mock_get):
+    @mock.patch('builtins.open', new_callable=mock.mock_open)  # Mock open to avoid writing a file
+    def test_scrape_books_to_scrape(self, mock_open, mock_get):
         # Sample HTML data (you can add more complex or realistic HTML for better testing)
         sample_html = """
         <html>
@@ -24,28 +24,22 @@ class TestScrapingFunction(unittest.TestCase):
             </body>
         </html>
         """
-
+    
         # Mock the HTTP response to return our sample HTML
         mock_response = mock.Mock()
         mock_response.text = sample_html
         mock_response.status_code = 200
         mock_get.return_value = mock_response
-
+    
         # Call the scraping function
         df = scrape_books_to_scrape()
 
-        # Check if the dataframe contains the expected data
-        self.assertEqual(len(df), 2)  # We expect two books in the mock response
-        self.assertEqual(df['title'].iloc[0], 'Book Title 1')
-        self.assertEqual(df['price'].iloc[0], '£51.77')
-        self.assertEqual(df['availability'].iloc[0], 'In stock')
-        self.assertEqual(df['link'].iloc[0], 'https://books.toscrape.comcatalogue/book-1')
+        # Check that open was called to write the debug file
+        mock_open.assert_called_with("data/debug.html", "w", encoding="utf-8")
 
-        # Test the second book as well
-        self.assertEqual(df['title'].iloc[1], 'Book Title 2')
-        self.assertEqual(df['price'].iloc[1], '£53.74')
-        self.assertEqual(df['availability'].iloc[1], 'In stock')
-        self.assertEqual(df['link'].iloc[1], 'https://books.toscrape.comcatalogue/book-2')
+        # Check the result
+        self.assertEqual(len(df), 2)  # Expecting 2 books from the mock HTML
+        self.assertEqual(df.iloc[0]["title"], "Book Title 1")  # Check the title of the first book
 
 if __name__ == '__main__':
     unittest.main()
